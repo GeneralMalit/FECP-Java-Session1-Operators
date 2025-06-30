144f912065b8caabd47be91371c94984c3b9de99
General Malit <general.malit@gfitftlapt04013.ad.mynt.xyz>
2025-06-30 15:06:01 +0800

    solution with updated test cases 5/5

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FECP5-1005-Lab01
+
+### Celsius to Fahrenheit Converter
+
+![result.png](result.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,11 @@ import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter temperature (in C): ");
+        double temperatureCelsius = sc.nextDouble();
+        double temperatureFahrenheit = temperatureCelsius * 9 / 5 + 32;
+        System.out.printf("Temperature in Fahrenheit: %.2f", temperatureFahrenheit);
 
     }
 }
